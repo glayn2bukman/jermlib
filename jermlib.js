d9773b021e31d02b44278390b18ec7673e203b4b
glayn2bukman <glayn2bukman@gmail.com>
2017-08-16 15:39:12 +0300

    Add files via upload

--- jermlib.js
+++ jermlib.js
@@ -458,6 +458,311 @@ function jermRemoveTab(tabbedPanelElement, TabIndex)
 
 }
 
+// jermGause custom widget...
+function jermGaugeProgress(value)
+{
+    value = value || this.increment;
+    
+    value = this.value + value;
+    
+    value = (this.loop && (value>this.maxValue)) ? this.minValue : value;
+        
+    this.setValue(value);
+}
+function jermGaugeShowLabel(bool)
+{
+    bool = bool || true;
+    
+    if ((typeof bool)!="boolean") {bool=true;}
+
+    if (bool) {this.labelElement.style.diplay = "block";}
+    else {this.labelElement.style.diplay = "none";}
+}
+function jermGaugeBorderRadius(radius)
+{
+    radius = radius || "0px";
+    this.gaugeContainer.style.borderRadius = radius;
+    this.barElement.style.borderRadius = radius;
+}
+function jermGaugeValue(value)
+{
+    if (value==null){return 0;}
+    value = (value>this.maxValue) ? this.maxValue : value;
+    
+    this.value = value;
+    
+    if (this.orientation=="vertical")
+    {
+        this.barElement.style.height = (((this.value-this.minValue)/(this.maxValue-this.minValue))*100)+"%";
+    }
+    else{this.barElement.style.width = (((this.value-this.minValue)/(this.maxValue-this.minValue))*100)+"%";}
+
+    if (this.label.show)
+    {
+        if (this.label.percentage)
+        {
+            this.labelElement.innerHTML = (((this.value-this.minValue)/(this.maxValue-this.minValue))*100).toFixed(this.label.dp)+"%";
+        }
+        else{this.labelElement.innerHTML = value + this.label.unit; }        
+
+        this.setLabelColor(this.label.color); /* its possible that this value was changed through 
+                                                 javascript from its initial value YET gauge.label.levels==[]!*/
+    }
+    
+    // now change bar and label color accordingly...
+    if (this.label.show && (this.label.levels.length>0))
+    {
+        var label_level_found = false;
+        for (var i=0; i<this.label.levels.length; i++)
+        {
+            if (this.value<=this.label.levels[i][0])
+            {
+                this.setLabelColor(this.label.levels[i][1]);
+                label_level_found = true;
+                break;
+            }
+        }        
+        if (!label_level_found) {this.setLabelColor(this.label.color); }
+    }
+    if (this.bar.levels.length>0)
+    {
+        var bar_level_found = false;
+        for (var i=0; i<this.bar.levels.length; i++)
+        {
+            if (this.value<=this.bar.levels[i][0])
+            {
+                this.setBarColor(this.bar.levels[i][1], this.bar.levels[i][2]);
+                bar_level_found = true;
+                break;
+            }
+        }        
+        if (!bar_level_found) {this.setLabelColor(this.bar.color[0], this.bar.color[1]); }
+    }
+    
+}
+function jermGaugeColor(color1, color2)
+{
+    if (color1==null){return 0;}
+    
+    if (color2==null){color2=color1;}
+    
+    this.gaugeContainer.style.backgroundColor = color1;
+
+    if (this.orientation=="horizontal")
+    {
+        this.gaugeContainer.style.background = "linear-gradient(to right, "+color1+", "+color2+")";        
+    }
+    else
+    {
+        this.gaugeContainer.style.background = "linear-gradient(to top, "+color1+", "+color2+")";        
+    }
+}
+function jermGaugeBarColor(color1, color2)
+{
+    if (color1==null){return 0;}
+    
+    if (color2==null){color2=color1;}
+
+    this.barElement.style.backgroundColor = color1; // incase browser doesn support gradient colors...
+
+    if (this.orientation=="horizontal")
+    {
+        this.barElement.style.background = "linear-gradient(to right, "+color1+", "+color2+")";        
+    }
+    else
+    {
+        this.barElement.style.background = "linear-gradient(to top, "+color1+", "+color2+")";        
+    }
+}
+function jermGaugeLabelColor(color)
+{
+    if (color==null){return 0;}
+    
+    this.labelElement.style.color = color;
+}
+
+function jermGauge(data)
+    /*
+        returns jermGauge Widget. to edit properties;
+        
+        parent elememnt: gauge.mom
+        gauge container: gauge.gaugeContainer (contains the bar and label...)
+        bar: gauge.barElement
+        label: gauge.labelElement
+
+    */
+{
+/*
+    data={
+        widget: // id of the widget(div) to contain the jermGauge
+        orientation:  // horizontal||vertical
+        borderRadius: // css border-radius; also used for the bar
+        minValue: // minimum value
+        maxValue: // maximum value        
+        increment: // if gause.progress(value) is called without argument, this value is used
+        loop: // bool; if value>maxValue and gauge.progress is called, value->minValue         
+        value: // initial value...  
+        color: // string(#hex) or array(["#hex-gradient-from", "#hex-gradient-to"])
+        
+        label: { // progress label
+                show: // bool
+                position: // below, top, center, left, right
+                color: // hex color
+                percentage: // bool; display %([max-value/max-min]*100%) not real value
+                dp: // how many decimal places to use (if percentage==true)
+                unit: // symbol(s) to be placed just after the value (just like % for percentages!)
+                
+                levels:[ // change attributes at given levels 
+                    [LEVEL1,COLOR], // color(COLOR) when value<=LEVEL1
+                    [LEVEL2,COLOR2], // color(COLOR2) when value<=LEVEL2
+                    ...
+                    [LEVELN,COLORN], // color(COLORN) when value<=LEVELN
+
+                    // else(value>LEVELN), use default label-color settings
+                ]
+            },
+            
+        bar:{ // progress/gauge bar
+                color:[COLOR_GRADIENT_FROM, COLOR_GRADIENT_TO], // if two values given, gradient color is used
+                
+                levels:[ // change attributes at given levels 
+                    // the COLOR_GRADIENT_TO can be left out to use a flat color instead of a gradient
+                    [LEVEL1,COLOR_GRADIENT_FROM, COLOR_GRADIENT_TO]
+                    [LEVEL2,COLOR_GRADIENT_FROM2, COLOR_GRADIENT_TO2]
+                    ...
+                    [LEVELN,COLOR_GRADIENT_FROMN, COLOR_GRADIENT_TON]
+
+                    // else(value>LEVELN), use default bar-color settings
+                 ],
+            },
+                      
+        });
+*/
+
+    // polish the object provided to create a new jermGauge object/widget
+    try{ 
+        if (data.widget==null){console.warn("invalid data provided for jermGauge! NO <widget> attribute found"); return this;}
+    }
+    catch(e){console.warn("invalid data provided for jermGauge! NO <widget> attribute found"); return this;}
+
+    if (document.getElementById(data.widget)==null){console.warn("no element found with ID <"+data.widget+">, jermGauge-widget construction dumped!"); return 0;}
+
+    data.orientation = data.orientation || "horizontal";
+
+    data.borderRadius = data.borderRadius || "0px";
+    data.minValue = data.minValue || 0;
+    data.maxValue = data.maxValue || 100;
+    data.increment = data.increment || 1;
+    data.loop = (data.loop==null) ? false : ((typeof data.loop)=="boolean" ? data.loop : false);
+
+    data.value = data.value || 0;
+    data.color = data.color || ["#ddd", "#eee"];
+
+    data.label = data.label || {};
+    data.label.show = (data.label.show==null) ? false : ((typeof data.label.show)=="boolean" ? data.label.show : false);
+    data.label.position = data.label.position || "center";
+    data.label.color = data.label.color || "#666";
+    data.label.percentage = (data.label.percentage==null) ? true : ((typeof data.label.percentage)=="boolean" ? data.label.percentage : true);
+    data.label.dp = data.label.dp || 0;
+    data.label.levels = data.label.levels || [];
+    data.label.unit = data.label.unit || "";
+
+    data.bar = data.bar || {};
+    data.bar.color = data.bar.color || ["#444", "#ddd"];
+    data.bar.levels = data.bar.levels || [];
+
+    // methods
+    data.progress = jermGaugeProgress;
+    data.showLabel = jermGaugeShowLabel;
+    data.setBorderRadius = jermGaugeBorderRadius;
+    data.setValue = jermGaugeValue;
+    data.setColor = jermGaugeColor;
+    data.setLabelColor = jermGaugeLabelColor;
+    data.setBarColor = jermGaugeBarColor;
+
+
+    // gauge.mom
+    data.mom = document.getElementById(data.widget);
+    
+    // gauge.gaugeContainer
+    data.gaugeContainer = document.createElement("div");
+    data.gaugeContainer.style.position = "absolute";
+    data.gaugeContainer.style.left = "0px";
+    data.gaugeContainer.style.top = "0px";
+    data.gaugeContainer.style.width = "100%";
+    data.gaugeContainer.style.height = "100%";
+    data.gaugeContainer.style.border = "1px solid";
+    data.gaugeContainer.style.borderRadius = data.borderRadius;
+
+    if ((typeof data.color)=="string")
+    {
+        data.gaugeContainer.style.backgroundColor = data.color;
+    }
+    else
+    {
+        data.gaugeContainer.style.background = "linear-gradient(to right, "+data.color[0]+", "+data.color[1]+")";        
+    }
+
+    // gauge.bar
+    data.barElement = document.createElement("div");
+    data.barElement.style.position = "absolute";
+    data.barElement.style.left = "0px";
+    data.barElement.style.border = "0px solid";
+    data.barElement.style.borderRadius = data.borderRadius;
+
+    if (data.orientation=="horizontal")
+    {
+        data.barElement.style.width = (((data.value-data.minValue)/(data.maxValue-data.minValue))*100)+"%";
+        data.barElement.style.height = "100%";
+        data.barElement.style.top = "0px";
+    }
+    else
+    {
+        data.barElement.style.height = (((data.value-data.minValue)/(data.maxValue-data.minValue))*100)+"%";
+        data.barElement.style.width = "100%";
+        data.barElement.style.bottom = "0px";
+    }
+
+    // gauge.labelElement
+    data.labelElement = document.createElement("div");
+    data.labelElement.style.position = "absolute";
+    data.labelElement.style.left = "0px";
+    data.labelElement.style.textAlign = "center";
+    data.labelElement.style.fontSize = "80%";
+    data.labelElement.style.height = "25px";
+
+
+    if (data.label.position=="center") 
+    {
+        if (data.orientation=="horizontal"){data.labelElement.style.left = "48%";}
+        else{data.labelElement.style.top = "48%";}
+    }
+    else if ((data.label.position=="bottom") || (data.labelElement.position=="left")) 
+    {
+        if (data.orientation!="horizontal"){data.labelElement.style.bottom = "0px";}
+    }
+    else if ((data.label.position=="top") || (data.labelElement.position=="right")) 
+    {
+        if (data.orientation=="horizontal"){data.labelElement.style.right = "100%";}
+        else{data.labelElement.style.top = "0px";}
+    }
+
+    if (data.orientation=="horizontal"){data.labelElement.style.lineHeight = data.mom.style.height;}
+    else {data.labelElement.style.margin = "auto"; data.labelElement.style.width="100%";}
+
+    data.gaugeContainer.appendChild(data.barElement);
+    data.gaugeContainer.appendChild(data.labelElement);
+    data.mom.appendChild(data.gaugeContainer);
+
+    // effect any changes in the bar/label due to the initial settings given...
+    data.showLabel(data.label.show);
+    data.setLabelColor(data.label.color);
+    if ((typeof data.bar.color)=="object"){data.setBarColor(data.bar.color[0], data.bar.color[1]);}
+    else{data.setBarColor(data.bar.color);}
+    data.setValue(data.value);
+ 
+    return data; 
+}
 
 // init all jermlib custom elements/classes...
 function jermlibInitAll()
